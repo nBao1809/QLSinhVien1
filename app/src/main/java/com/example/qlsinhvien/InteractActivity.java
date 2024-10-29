e783bc64e859e5979eddb683644be6ee1a427c98
Truong Hoang Khang <khang692004@gmail.com>
2024-10-29 16:51:18 +0700

    Login UI & No DB Sign In, Out interaction

--- app/src/main/java/com/example/qlsinhvien/InteractActivity.java
+++ app/src/main/java/com/example/qlsinhvien/InteractActivity.java
@@ -1,6 +1,9 @@
 package com.example.qlsinhvien;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class InteractActivity extends AppCompatActivity {
+Button btSignOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_interact);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btSignOut=findViewById(R.id.btSignout);
+        btSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(InteractActivity.this,LoginActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
         });
     }
 }
